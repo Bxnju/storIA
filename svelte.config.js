ed9e685814a0e3c9dc5a55c0cf061191bd859b8e
Andrés Benjumea Reinoso <97989812+Bxnju@users.noreply.github.com>
2024-11-12 12:51:18 -0500

    fix: update svelte.config.js for vercel deployement

--- svelte.config.js
+++ svelte.config.js
@@ -3,10 +3,17 @@ import adapter from '@sveltejs/adapter-auto';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+		prerender: {
+			// Maneja errores HTTP durante el prerender
+			handleHttpError: ({ status, path }) => {
+				if (status === 404) {
+					console.warn(`❌ Skipping prerender for: ${path} (404)`);
+					return; // Ignora el error 404 y continúa con el resto del prerender
+				}
+				// Opcionalmente, puedes manejar otros códigos de error si es necesario
+			}
+		}
 	}
 };
 
